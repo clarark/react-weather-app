e4fe7b728c79c715a4fd72e4d36a8e0a57713592
clarark <clara.rk@hotmail.com>
2021-07-06 14:15:40 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Clara Redshaw Kranich and is {""}
+      <a href="https://github.com/clarark/react-weather-app" target="_blank" rel="noreferrer">
+        open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
